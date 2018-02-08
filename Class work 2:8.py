0c06c21997656d388a4fd91630ca3907bd0aedf3
agordo25 <34726753+agordo25@users.noreply.github.com>
2018-02-08 14:50:01 -0500

    Add files via upload

---
+++ Class work 2:8.py
@@ -0,0 +1,36 @@
+***
+#Design a function that prints the even numbers in a range of numbers
+def even(n): 
+    for number in range(1, n+1):
+        if number %2 != 2:
+            print(number, "is even")
+
+even(100)
+
+#function that returns the addition of 2 numbers
+def sum_of_numbers(x,y,z):
+    x=int("Enter a number")
+    y=int("Enter a second number")
+    z=int("Enter a third number")
+    return x+y+z
+
+
+import turtle as t
+t,foward(50)
+t.left(90)
+t.foward(100)
+
+import turtle as t
+def drawRectnagle(t,w,h)
+    for i in range(2):
+        t.foward(w)
+        t.left(90)
+        t.forward(h)
+        t.left(90)
+
+drawRectangle(t,300,150)
+
+def drawSquare(tx,sz):
+    drawRectangle(tx,sz, sz)
+
+drawSquare(t, 50)
